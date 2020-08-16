dda2af56a23028bf536fcdd327f6e6a54fe6a35a
Théophile Louvart <th.louvart29@gmail.com>
2020-08-16 19:43:39 +0200

    Add files via upload

--- Deploy Services/Flask App/flask_pp.py
+++ Deploy Services/Flask App/flask_pp.py
@@ -6,19 +6,42 @@ Created on Tue Aug 11 19:08:09 2020
 """
 #Imports
 from flask import Flask, render_template, request, redirect
+#DB import
 from flask_sqlalchemy import SQLAlchemy
+
+#Forms imports
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+
+#Security password import
+from werkzeug.security import generate_password_hash, check_password_hash
+  
+#Manage login system import
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+
 from datetime import datetime
 
 # Create a basic flask app
 app =  Flask(__name__)
 
+app.config['SECRET_KEY'] = "thisissecret"
 # Tell flask app where db is stored
 #Using sqlite before production deployement
 # Sqlite store locally in file 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+Bootstrap(app)
 # Link app and db
 db = SQLAlchemy(app)
+
+
+#Initiliasation of login management
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 #Designing the db through class
 
@@ -35,7 +58,27 @@ class BlogPost(db.Model):
     def __repr__(self):
         return 'Blog post' + str(self.id)
 
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
 
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+
+#Designing the login system
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])    
+    remember = BooleanField('Remember me')
+    
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Email('Invalid Email'), Length(max=50)])
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)]) 
 
 #get cam name from txt
 # d = []
@@ -46,9 +89,48 @@ class BlogPost(db.Model):
 
 
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/signup',methods=['GET','POST'])
+def sign_up():
+    form = RegisterForm()
+    
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = Users(username= form.username.data, email= form.email.data, password= hashed_password)
+        db.session.add(new_user)
+        db.session.commit() 
+        return "<h1> user created </h1>"
+    
+    return render_template('signup.html', form=form)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    
+    if form.validate_on_submit():
+        user = Users.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect('/dashboard')
+        return '<h1>Invalid username or password</h1>'
+    return render_template('login.html', form=form) 
+
+@app.route('/logout')
+def log_out():
+    logout_user() 
+    return redirect('/')
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', name = current_user.username)
+
+
 
 @app.route('/posts',methods=['GET','POST'])
 def posts():
